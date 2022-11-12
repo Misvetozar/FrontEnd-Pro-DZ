c578ae54ff3f7d76ca7dd21a5c479b9fa784c446
Misvetozar <misvetozar@gmail.com>
2022-11-12 19:07:35 +0200

    Homework 13

--- homework13/script.js
+++ homework13/script.js
@@ -1,18 +1,17 @@
-const SIZE_SMALL = { price: 50, calloties: 20};
-const SIZE_MIDDLE = { price: 75, calloties: 30};
-const SIZE_BIG = { price: 100, callories: 40};
-
-const TOPPING_MAYO = { price: 20, callories: 5};
-const TOPPING_SALAD = { price: 20, callories: 5};
-const TOPPING_CHEESE= { price: 10, callories: 20};
-const TOPPING_POTATO = { price: 15, callories: 10};
-const TOPPING_HERBAL = { price: 15, callories: 0};
-
-
 function Hamburger({price, callories}) {
     this._price = price;
     this._callories = callories;
 }
+
+Hamburger.SIZE_SMALL = { price: 50, calloties: 20};
+Hamburger.SIZE_MIDDLE = { price: 75, calloties: 30};
+Hamburger.SIZE_BIG = { price: 100, callories: 40};
+
+Hamburger.TOPPING_MAYO = { price: 20, callories: 5};
+Hamburger.TOPPING_SALAD = { price: 20, callories: 5};
+Hamburger.TOPPING_CHEESE= { price: 10, callories: 20};
+Hamburger.TOPPING_POTATO = { price: 15, callories: 10};
+Hamburger.TOPPING_HERBAL = { price: 15, callories: 0};
 
 
 Hamburger.prototype.addTopping = function ({price, callories}) {
