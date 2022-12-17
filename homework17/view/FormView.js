b6165f5eaf306d48f97930881cab06fdf774f57b
Svetozar Mylovanov <misvetozar@gmail.com>
2022-12-17 21:41:19 +0200

    complete

--- homework17/view/FormView.js
+++ homework17/view/FormView.js
@@ -48,11 +48,3 @@ class FormView {
             
         }
 }
-
-function htmlToElement(html){
-   const container =  document.createElement('div');
-
-    container.innerHTML = html;
-
-    return container.children[0];
-}
