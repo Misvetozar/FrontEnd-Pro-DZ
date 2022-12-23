036319881a48ad2b2870bd5778946a2ae46645c1
Svetozar Mylovanov <misvetozar@gmail.com>
2022-12-24 00:40:14 +0200

    done

---
+++ homework20/app.js
@@ -0,0 +1,6 @@
+const calculator = require('./calculator');
+
+console.log('add', calculator.add(2,10));
+console.log('sub', calculator.sub(12,5));
+console.log('mult', calculator.mult(4,7));
+console.log('div', calculator.div(28,2));
